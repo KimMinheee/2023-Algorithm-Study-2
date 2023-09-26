54935c4e1fe9f04c9c9096dda456c8d64f51971d
KimMinhee <99alsgmlalsl@naver.com>
2023-09-26 23:43:00 +0900

    공백 정리

--- minhee/BFSDFS/B1463_1로만들기.java
+++ minhee/BFSDFS/B1463_1로만들기.java
@@ -12,44 +12,45 @@ public class B1463_1로만들기 {
         int X = Integer.parseInt(br.readLine());
         int count = 0;
 
-        int answer = bfs(X,count);
+        int answer = bfs(X, count);
         System.out.println(answer);
         br.close();
     }
 
-    private static int bfs(int X, int count){
+    private static int bfs(int X, int count) {
         Queue<Info> que = new LinkedList<>();
-        boolean[] visited = new boolean[X+1];
+        boolean[] visited = new boolean[X + 1];
 
         visited[X] = true;
-        que.add(new Info(X,0));
+        que.add(new Info(X, 0));
 
-        while(!que.isEmpty()){
+        while (!que.isEmpty()) {
             Info tmpInfo = que.poll();
             int tmpNum = tmpInfo.num;
             int tmpCount = tmpInfo.count;
-            if(tmpInfo.num == 1){
+            if (tmpInfo.num == 1) {
                 return tmpCount;
             }
 
-            if(tmpNum % 3 == 0 && !visited[tmpNum/3]){
-                que.add(new Info(tmpNum/3,tmpCount+1));
+            if (tmpNum % 3 == 0 && !visited[tmpNum / 3]) {
+                que.add(new Info(tmpNum / 3, tmpCount + 1));
             }
-            if(tmpNum % 2 == 0 && !visited[tmpNum/2]){
-                que.add(new Info(tmpNum/2, tmpCount+1));
+            if (tmpNum % 2 == 0 && !visited[tmpNum / 2]) {
+                que.add(new Info(tmpNum / 2, tmpCount + 1));
             }
-            if(!visited[tmpNum-1]){
-                que.add(new Info(tmpNum-1, tmpCount+1));
+            if (tmpNum - 1 > 0 && !visited[tmpNum - 1]) {
+                que.add(new Info(tmpNum - 1, tmpCount + 1));
             }
         }
         return 0;
     }
 }
-class Info{
+
+class Info {
     int num;
     int count;
 
-    public Info(int num, int count){
+    public Info(int num, int count) {
         this.num = num;
         this.count = count;
     }
