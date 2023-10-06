7ec0cdb26239913eb0c14f7f7de2b8b4c9d8aec6
jaehwan97 <107037978+jaehwan97@users.noreply.github.com>
2023-10-06 16:21:16 +0900

    //[PGM-87390]배열자르기 jaehwan solved

---
+++ jaehwan/프로그래머스_실전풀이/배열자르기.java
@@ -0,0 +1,13 @@
+class 배열자르기 {//[PGM-87390]배열자르기 jaehwan solved
+    public int[] solution(int n, long left, long right) {
+         int[] answer = new int[(int)(right - left) + 1];
+        
+        for(int i = 0; i < answer.length; i++){
+            int row = (int)((i + left) / n) + 1; 
+            int col = (int)((i + left) % n) + 1;
+            answer[i] = Math.max(row, col); //행렬
+        }
+        return answer;
+    }
+}
+//행과 열중 가장 큰수로 계속 갱신되는 일차원 행렬 값을 가지게된다 1,2,3,2,2,3,3,3,3
