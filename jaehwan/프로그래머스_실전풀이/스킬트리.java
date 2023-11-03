9f00ce441e92200ee9f6cfb954bc76189b470af6
jaehwan97 <107037978+jaehwan97@users.noreply.github.com>
2023-11-03 19:16:34 +0900

    [PGM-49993]스킬트리 jaehwan

--- jaehwan/프로그래머스_실전풀이/스킬트리.java
+++ jaehwan/프로그래머스_실전풀이/스킬트리.java
@@ -8,8 +8,9 @@ public class 스킬트리 {//[PGM-49993]스킬트리 jaehwan
         public int solution(String skill, String[] skill_trees) {
             int answer = 0;
             for(String s:skill_trees){
-                String str=s.replaceAll("[^"+skill+"]","");
-                if(skill.indexOf(str)==0){
+                //정규식[^...] skill에 포함된 문자들을 제외한 모든 문자를 선택하도록 정의
+                String str=s.replaceAll("[^"+skill+"]",""); //다 날려버림
+                if(skill.indexOf(str)==0){//indexOf(String)는 인자로 전달된 String의 index를 리턴
                     answer++;
                 }
             }
